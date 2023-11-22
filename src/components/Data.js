eac0e8f102c865b26469a793cc09e4328f08a15f
Prince-kumar-pk <princekumar92@gmail.com>
2023-11-22 10:32:38 +0530

    update json data

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,35 +1,35 @@
 var Data = {
-    Name: "Prince Kumar",
+    Name: "Anuj Gosalia",
     coverpic: "https://libg.s3.us-east-2.amazonaws.com/download/Paradise-Beach.jpg",
-    profilepic: "https://media.licdn.com/dms/image/D4D03AQGrVF_qOzCXLw/profile-displayphoto-shrink_400_400/0/1679564204542?e=1705536000&v=beta&t=qLpN7m5wzSMNjvMMBFOXHc8HW46-XzsNKMpPKJTb8go",
-    followers: "200",
+    profilepic: "https://media.licdn.com/dms/image/D4D03AQEXdD5HHqzN3g/profile-displayphoto-shrink_800_800/0/1667373388482?e=2147483647&v=beta&t=3OsbjB3Clq9SXxS5hiXldylkaEX03X3FbtmNpyTMDVo",
+    followers: "6482",
     following: "525",
-    designation: "SDE-I at Terribly Tiny Tales",
-    link: "https://www.linkedin.com/in/prince--kr/",
+    designation: "Co-founder & CEO at Terribly Tiny Tales",
+    link: "https://www.instagram.com/anujgosalia",
     posts: [
         {
             header: "A changing World Order",
             content: "The world is changing at a feverish pace. Friends,colleagues and everyone engaged in knowladge work are begining to sense this, including Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad hic totam cum, reiciendis modi? Ipsam illum reiciendis deserunt facere!", 
-            tag: "tag",
-            byname: "by someone",
-            date: "November 21",
-            timesRead: "10",
-            views: "2324"
+            tag: "musing",
+            byname: "by anujgosalia",
+            date: "August 2",
+            timesRead: "2",
+            views: "102"
         },
         {
-            header: "Another Post Title",
-            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A harum voluptatem maxime est ex. Et, est reiciendis! Veniam soluta adipisci pariatur a ab nobis rem tempore nostrum voluptatum quasi aliquam debitis, similique ipsam esse quae est? Adipisci tempora fuga repellendus?",
-            tag: "tag",
-            byname: "anotherauthor",
-            date: "August 4",
-            timesRead: "20",
-            views: "5678"
+            header: "India v/s Australia",
+            content: " Think about it - this cricket series was akin to therapy. Laying our childhood trauma up top, working with it slowly and surely over two movment Lorem ipsum dolor sit amet, consectetur adipisicing elit. A harum voluptatem maxime est ex. Et, est reiciendis! Veniam soluta adipisci pariatur a ab nobis rem tempore nostrum voluptatum quasi aliquam debitis, similique ipsam esse quae est? Adipisci tempora fuga repellendus?",
+            tag: "thought",
+            byname: "by anujgosalia",
+            date: "january 4",
+            timesRead: "1",
+            views: "156"
         },
         {
-            header: "Another Post Title",
+            header: "write To Build",
             content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A harum voluptatem maxime est ex. Et, est reiciendis! Veniam soluta adipisci pariatur a ab nobis rem tempore nostrum voluptatum quasi aliquam debitis, similique ipsam esse quae est? Adipisci tempora fuga repellendus?",
-            tag: "tag",
-            byname: "anotherauthor",
+            tag: "thought",
+            byname: "anujgosalia",
             date: "August 4",
             timesRead: "20",
             views: "5678"
@@ -53,7 +53,6 @@ var Data = {
             views: "5678"
         },
        
-        // You can add more posts here
     ]
 };
 
